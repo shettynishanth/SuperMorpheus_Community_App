f6617a054272210603ec4b5ac4346f15bcf5c1c1
Nishanth Shetty <129318811+shettynishanth@users.noreply.github.com>
2025-04-06 21:06:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MemberList from './components/MemberList/MemberList';
+import AddMemberForm from './components/AddMemberForm/AddMemberForm';
+import { useMembers } from './hooks/useMembers';
+import styles from './App.module.css';
+// import MemberCard from './components/MemberCard/MemberCard';
 
-function App() {
+const App: React.FC = () => {
+  const { addMember } = useMembers();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>SuperMorpheus Community</h1>
       </header>
+      
+      <main className={styles.mainContent}>
+        <MemberList />
+      <AddMemberForm onAddMember={addMember} />
+
+        
+      </main>
     </div>
   );
-}
+};
 
 export default App;
